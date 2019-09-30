15dacff7b3e57de2cd31e06bfc681f979604b43a
MayureshSparkt <mayuresh@sparkt.in>
2019-09-30 12:46:05 +0530

    New Changes

--- MC.Web/Controllers/HomeController.cs
+++ MC.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MC.BusinessAccess.Interface;
+using MC.Entities;
+using MC.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,35 @@ namespace MC.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ICityBusinessAccess _cityBusinessAccess;
+
+        private readonly IAuditionBusinessAccess _auditionBusinessAccess;
+        public HomeController(ICityBusinessAccess cityBusinessAccess, IAuditionBusinessAccess auditionBusinessAccess)
+        {
+            _auditionBusinessAccess = auditionBusinessAccess;
+            _cityBusinessAccess = cityBusinessAccess;
+        }
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+
+                HomeViewModel homeViewModel = new HomeViewModel();
+                List<CityModel> city = _cityBusinessAccess.GetAuditionCity().OrderBy(c => c.City).ToList();
+                if (city.Count > 0)
+                {
+                    homeViewModel = GetViewModel(city.FirstOrDefault().CityId);
+                }
+                ViewBag.City = city;
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+
+                //LogService.Log(ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
         }
 
         public ActionResult About()
@@ -26,5 +55,43 @@ namespace MC.Web.Controllers
 
             return View();
         }
+
+        public HomeViewModel GetViewModel(int cityId)
+        {
+            try
+            {
+                HomeViewModel homeViewModel = new HomeViewModel();
+                if (cityId != 0)
+                {
+                    homeViewModel.CityId = cityId;
+                    homeViewModel.AuditionList = _auditionBusinessAccess.GetAuditionByCityId(cityId);
+                }
+                return homeViewModel;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+
+        public ActionResult GetLocationVenue(int cityId)
+        {
+            try
+            {
+                List<AuditionModel> auditionModels = _auditionBusinessAccess.GetAuditionByCityId(cityId);
+                return PartialView("_AuditionDayLocation", auditionModels);
+            }
+            catch (Exception ex)
+            {
+
+                //LogService.Log(ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
+
+        }
+
     }
 }
---
+++ MC.Web/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MC.Web.Models
+{
+    public class HomeViewModel
+    {
+        public int CityId { get; set; }
+
+        public List<AuditionModel> AuditionList { get; set; }
+    }
+}
